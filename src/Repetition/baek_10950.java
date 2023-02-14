56ea566b101cb29d415ec3430c566ca591146718
iiuuyy6654@gmail.com <iiuuyy6654@gmail.com>
2023-02-14 16:03:59 +0900

    SelfNum

---
+++ src/Repetition/baek_10950.java
@@ -0,0 +1,22 @@
+package Repetition;//baek_23_10950
+
+import java.util.Scanner;
+
+public class baek_10950 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int T = scanner.nextInt();
+
+        int A, B;
+
+        for (int i = 1; i<=T; i++) {
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+
+        System.out.println(A+B);
+        }
+
+        scanner.close();
+    }
+}
---
+++ src/Repetition/baek_2739.java
@@ -0,0 +1,16 @@
+package Repetition;//baek_22_2739
+
+import java.util.Scanner;
+
+public class baek_2739 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 1; i<=9; i++) {
+            System.out.println(N + " * " + i + " = " +(N * i));
+        }
+    }
+}
